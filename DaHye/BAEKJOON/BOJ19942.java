99eba4c5b63f4e2b8e5fede3b1434df48fe3f751
KodaHye <kodhdh09@gmail.com>
2023-02-26 23:12:15 +0900

    add: '다이어트' 문제풀이 완료

--- DaHye/BAEKJOON/BOJ19942.java
+++ DaHye/BAEKJOON/BOJ19942.java
@@ -7,8 +7,6 @@ package BAEKJOON;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
