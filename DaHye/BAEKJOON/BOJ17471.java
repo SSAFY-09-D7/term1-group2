4484d18eb48e87f827c45e1ade9de1ff3e6ec211
KodaHye <kodhdh09@gmail.com>
2023-03-02 01:53:53 +0900

    add: '게리맨더링' 문제풀이 완료

--- DaHye/BAEKJOON/BOJ17471.java
+++ DaHye/BAEKJOON/BOJ17471.java
@@ -9,7 +9,6 @@ package BAEKJOON;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
