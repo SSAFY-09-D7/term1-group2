6a7074147c45464407a584cfb50fd659e667a447
KodaHye <kodhdh09@gmail.com>
2023-03-19 20:25:52 +0900

    add: '빙산' 문제풀이 완료

--- DaHye/BAEKJOON/BOJ2573.java
+++ DaHye/BAEKJOON/BOJ2573.java
@@ -1,7 +1,4 @@
 package BAEKJOON;
-/*
- * 빙산
- */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -10,13 +7,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+ * 빙산
+ */
+
 public class BOJ2573 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int N, M, time, map[][], afterMap[][], ice;
-	static int dr[] = {-1, 1, 0, 0};
+	static int N, M, time, map[][], copyMap[][];
+	static int dr[] = {1, -1, 0, 0};
 	static int dc[] = {0, 0, -1, 1};
-	static Queue<int[]> iceCount;
+	
 	public static void main(String[] args) throws Exception {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
@@ -24,74 +25,93 @@ public class BOJ2573 {
 		
 		map = new int[N][M];
 		
-		for(int r = 0; r < map.length; r++) {
+		for (int r = 0; r < map.length; r++) {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < map[r].length; c++) {
 				map[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		while(true) {
-			ice = 0;
+		// 빙산의 개수를 세는 check함수
+		while(!check()) {
 			time++;
 			
-			afterMap = new int[N][M];
-			
-			iceCount = new LinkedList<>();
-			
-			for (int r = 0; r < map.length; r++) {
-				for (int c = 0; c < map[r].length; c++) {
-					if(map[r][c] != 0) {
-						iceCount.offer(new int[] {r, c});
-					}
-				}
+			// 조건 잘 확인하기!
+			// 전부 다 녹을 떄까지 두 덩어리 이상으로 분리되지 않으면 0 출력
+			if(checkMap()) {
+				time = 0;
+				break;
 			}
 			
-			afterTime();
-			for (int r = 0; r < afterMap.length; r++) {
-				for (int c = 0; c < afterMap[r].length; c++) {
-					if(afterMap[r][c] != 0) {
-						map = copyArr();
-						bfs(r, c);
-					}
-				}
-			}
-			
-			if(ice == 2) break;
+			// 빙산 녹이기
+			melt();
+
 		}
-		
 		System.out.println(time);
-
 	}
 
-	private static void afterTime() {
-		while(!iceCount.isEmpty()) {
-			int[] point = iceCount.poll();
-			int count = 0;
-			
-			for(int i = 0; i < 4; i++) {
-				int nr = point[0] + dr[i];
-				int nc = point[1] + dc[i];
-				
-				if(!check(nr, nc))  continue;
-				if(map[nr][nc] != 0) {
-					count++;
+	private static void melt() {
+		
+		Queue<int[]> ice = new LinkedList<>();
+		
+		for (int r = 0; r < map.length; r++) {
+			for (int c = 0; c < map[r].length; c++) {
+				if(map[r][c] != 0) {
+					// 주변 빙산의 개수
+					int countIce = 0;
+					
+					for(int i = 0; i < 4; i++) {
+						int nr = r + dr[i];
+						int nc = c + dc[i];
+						
+						if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+						if(map[nr][nc] == 0) countIce++;
+					}
+					
+					ice.add(new int[] {r, c, countIce});
 				}
 			}
+		}
+		
+		while(!ice.isEmpty()) {
+			// 빙산를 녹이기 위해 빙산의 좌표, 바다로 둘러쌓인 면의 개수
+			int[] calIce = ice.poll();
 			
-			afterMap[point[0]][point[1]] = map[]
+			// 빙산의 r 좌표
+			int tr = calIce[0];
+			// 빙산의 c 좌표
+			int tc = calIce[1];
+			
+			int tIce = map[tr][tc] - calIce[2];
+			map[tr][tc] = tIce >= 0 ? tIce : 0; 
 		}
 	}
 
-	private static int[][] copyArr() {
-		int [][] tmpArr;
-		
-		tmpArr = new int[N][M];
-		
-		for (int r = 0; r < tmpArr.length; r++) {
-			tmpArr[r] = Arrays.copyOf(afterMap[r], afterMap[r].length);
+	private static boolean checkMap() {
+		for (int r = 0; r < map.length; r++) {
+			for (int c = 0; c < map[r].length; c++) {
+				if(map[r][c] != 0) return false;				
+			}
 		}
-		return tmpArr;
+		return true;
+	}
+
+	private static boolean check() {
+		int count = 0;
+		// map의 복사본
+		copyMap = copyArr();
+		
+		for (int r = 0; r < copyMap.length; r++) {
+			for (int c = 0; c < copyMap[r].length; c++) {
+				if(copyMap[r][c] != 0) {
+					count++;
+					bfs(r, c);
+				}
+			}
+		}
+		
+		// 빙산의 개수가 2개 이상이라면 true를 반환
+		return count >= 2 ? true : false;
 	}
 
 	private static void bfs(int r, int c) {
@@ -102,38 +122,35 @@ public class BOJ2573 {
 		v[r][c] = true;
 		
 		while(!queue.isEmpty()) {
-			int[] point = queue.poll();
+			int point[] = queue.poll();
 			
-			for(int i= 0; i < 4; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
+			for(int i = 0; i < 4; i++) {
+				int nr = point[0] + dr[i];
+				int nc = point[1] + dc[i];
 				
-				if(!check(nr, nc)) continue;
-				if(!v[nr][nc] && map[nr][nc] != 0) {
+				if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+				if(copyMap[nr][nc] != 0 && !v[nr][nc]) {
+					copyMap[nr][nc] = 0;
 					v[nr][nc] = true;
-					map[nr][nc] = 0;
 					queue.add(new int[] {nr, nc});
 				}
 			}
-			
 		}
 	}
 
-	private static int func(int r, int c) {
-		int count = 0;
+	private static int[][] copyArr() {
+		int tmp[][] = new int[N][M];
 		
-		for(int i = 0; i < 4; i++) {
-			int nr = map[r][c] + dr[i];
-			int nc = map[r][c] + dc[i];
-			
-			if(!check(nr, nc)) continue;
-			if(map[nr][nc] == 0) count++;
+		for (int r = 0; r < map.length; r++) {
+			tmp[r] = Arrays.copyOf(map[r], map[r].length);
 		}
-		
-		return count;
+		return tmp;
 	}
-
-	private static boolean check(int nr, int nc) {
-		return nr >= 0 && nr < N && nc >= 0 && nc < M;
+	
+	private static void print(int[][] map2) {
+		for (int r = 0; r < map2.length; r++) {
+			System.out.println(Arrays.toString(map2[r]));
+		}
+		System.out.println("==============");
 	}
 }
